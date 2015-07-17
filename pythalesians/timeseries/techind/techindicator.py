0cfec30ab348fce612a099db45ac6d2a6199be47
saeedamen <saeedamen@hotmail.com>
2015-07-17 18:16:04 +0100

    Removed SMA example

--- pythalesians/timeseries/techind/techindicator.py
+++ pythalesians/timeseries/techind/techindicator.py
@@ -167,38 +167,5 @@ class TechIndicator:
         return self._signal
 
 if __name__ == '__main__':
-
-    ###### Plot EUR/USD and GBP/USD 20D SMA
-
-    if True:
-        from pythalesians.market.loaders.lighttimeseriesfactory import LightTimeSeriesFactory
-        from pythalesians.market.requests.timeseriesrequest import TimeSeriesRequest
-
-        import datetime
-
-        time_series_request = TimeSeriesRequest(
-                start_date = "01 Jan 1970",                     # start date
-                finish_date = datetime.date.today(),            # finish date
-                freq = 'daily',                                 # daily data
-                data_source = 'quandl',                         # use Quandl as data source
-                tickers = ['EURUSD',                            # ticker (Thalesians)
-                           'GBPUSD'],
-                fields = ['close'],                                 # which fields to download
-                vendor_tickers = ['FRED/DEXUSEU', 'FRED/DEXUSUK'],  # ticker (Quandl)
-                vendor_fields = ['close'],                          # which Bloomberg fields to download
-                cache_algo = 'internet_load_return')                # how to return data
-
-        ltsf = LightTimeSeriesFactory()
-
-        daily_vals = ltsf.harvest_time_series(time_series_request)
-
-        techind = TechIndicator()
-
-        techind.create_tech_ind(daily_vals, 'SMA', periods = 20)
-
-        sma = techind.get_techind()
-        signal = techind.get_signal()
-
-        combine = daily_vals.join(sma, how = 'outer')
-
-        print(combine)
+    pass
+    # see pythalesians-examples/techindicator_examples

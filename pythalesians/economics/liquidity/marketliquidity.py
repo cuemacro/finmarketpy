0fb595c529593e712c39558cdf915e73a6fbef0a
saeedamen <saeedamen@hotmail.com>
2015-07-24 15:03:13 +0100

    Added license messages on economics package

--- pythalesians/economics/liquidity/marketliquidity.py
+++ pythalesians/economics/liquidity/marketliquidity.py
@@ -1,5 +1,24 @@
 __author__ = 'saeedamen'
 
+#
+# Copyright 2015 Thalesians Ltd. - http//www.thalesians.com / @thalesians
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and limitations under the License.
+#
+
+"""
+MarketLiquidity
+
+Calculates spread between bid/ask and also tick count.
+
+"""
+
 import pandas
 
 from pythalesians.util.configmanager import ConfigManager
@@ -9,7 +28,6 @@ from pythalesians.market.loaders.assets.fxcrossfactory import FXCrossFactory
 class MarketLiquidity:
 
     def __init__(self):
-        self.config = ConfigManager()
         self.logger = LoggerManager().getLogger(__name__)
         return
 

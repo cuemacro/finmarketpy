222babd33bf9c2c23d50bd02a9e8723bab36c7cd
saeedamen <saeedamen@hotmail.com>
2016-07-26 10:11:29 +0100

    Added vol surface example

--- pythalesians_examples/graphics/plotly_examples.py
+++ pythalesians_examples/graphics/plotly_examples.py
@@ -1,5 +1,24 @@
 __author__ = 'saeedamen'
 
+#
+# Copyright 2015 Thalesians Ltd. - http//www.thalesians.com / @thalesians
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and limitations under the License.
+#
+
+"""
+plotly_examples
+
+Shows how to plot using Plotly library (uses Jorge Santos' Cufflinks wrapper)
+
+"""
+
 import datetime
 from pythalesians.market.loaders.lighttimeseriesfactory import LightTimeSeriesFactory
 from pythalesians.market.requests.timeseriesrequest import TimeSeriesRequest
@@ -7,6 +26,23 @@ from pythalesians.timeseries.calcs.timeseriescalcs import TimeSeriesCalcs
 
 from pythalesians.graphics.graphs.plotfactory import PlotFactory
 from pythalesians.graphics.graphs.graphproperties import GraphProperties
+
+
+if True:
+    import pandas
+    df = pandas.read_csv("volsurface.csv")  # load a snapshot for a vol surface from disk
+
+    gp = GraphProperties()
+    gp.plotly_plot_mode = "offline_html"    # render Plotly plot locally (rather than via website)
+    gp.file_output = "volsurface.png"       # save as static PNG file
+    gp.html_file_output = "volsurface.html" # save as interactive HTML file
+
+    gp.title = "GBP/USD vol surface"
+    gp.color = 'Blues'
+
+    # plot surface with Plotly
+    pf = PlotFactory()
+    pf.plot_generic_graph(df, type = 'surface', adapter = 'cufflinks', gp = gp)
 
 if True:
 
@@ -31,10 +67,10 @@ if True:
 
     # plot first with PyThalesians and then Plotly (via Cufflinks)
     # just needs 1 word to change
-    # (although, note that AdapterCufflinks does have some extra parameters that can be set in
-    # GraphProperties)
-    gp.plotly_username = 'thalesians'
+    # (although, note that AdapterCufflinks does have some extra parameters that can be set in GraphProperties)
+    gp.plotly_username = 'thalesians'   # note: need to fill in Plotly API key on Constants and change this!
     gp.plotly_world_readable = True
+    gp.plotly_plot_mode = "online"      # will render on Plotly website
 
     pf = PlotFactory()
     pf.plot_generic_graph(df, type = 'line', adapter = 'pythalesians', gp = gp)
@@ -76,7 +112,7 @@ if True:
             columns = ["EURUSDV1M-1Y", "USDJPYV1M-1Y"])
 
     # resample fand calculate average over month
-    slope_monthly = slope.resample('M', how='mean')
+    slope_monthly = slope.resample('M').mean()
 
     slope_monthly.index = [str(x.year) + '/' + str(x.month) for x in slope_monthly.index]
 
@@ -93,4 +129,4 @@ if True:
     gp.y_title = 'Pc'
 
     # plot using Cufflinks
-    pf.plot_bar_graph(slope_monthly, adapter = 'bokeh', gp = gp)
+    pf.plot_bar_graph(slope_monthly, adapter = 'cufflinks', gp = gp)
